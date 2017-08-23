dcd0c43ce6d0f2d17eb40a7efd09c2f90a4dde28
Mai-Haotian <hm5xd@mail.missouri.edu>
2017-08-23 12:22:48 -0700

    Upload old

---
+++ menu.js
@@ -0,0 +1,41 @@
+//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) {
+		$('nav ul').removeAttr('style');
+	 }
+	});//end resize
+});//end ready
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
